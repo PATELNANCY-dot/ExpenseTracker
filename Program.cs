ee7f57775647af6393a2052c9a08dcf2a007e714
nancy patel <nancypatel8002@gmail.com>
2026-04-21 16:01:16 +0530

    Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// API ONLY
 builder.Services.AddControllers();
 
 // Swagger
@@ -10,16 +10,15 @@ builder.Services.AddSwaggerGen();
 // CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngular",
-        policy =>
-        {
-            policy.WithOrigins(
-                "http://localhost:4200",
-                "https://adorable-travesseiro-9337e2.netlify.app"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:4200",
+            "https://adorable-travesseiro-9337e2.netlify.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
@@ -32,11 +31,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();   // IMPORTANT
+
 app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
-//  IMPORTANT
-app.MapControllers();
+app.MapControllers();   // ONLY THIS
 
 app.Run();
